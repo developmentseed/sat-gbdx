8d6d6d2bb358e9dca4cbaca4fa4afcf15afcd25e
Rub21 <rub2106@gmail.com>
2018-07-11 17:10:11 -0500

    update

--- data/download.sh
+++ data/download.sh
@@ -5,7 +5,6 @@ featuresIds=$2
 
 # Filter the features which were evaluated as ok
 geokit filterbyprop $pathGeojson --prop id=$featuresIds > $DIR/tmp.geojson
-# mv $DIR/tmp.geojson > $pathGeojson
-# rm $DIR/tmp.geojson
-# sat-gbdx load $pathGeojson --download full
-
+rm $pathGeojson
+mv $DIR/tmp.geojson $pathGeojson
+sat-gbdx load $pathGeojson --download full
--- data/script.sh
+++ data/script.sh
@@ -7,28 +7,22 @@ mkdir $id
 wget https://s3.amazonaws.com/ds-data-projects/SEZ/sez-zone/$id.geojson -O $id/$id.geojson
 
 # Searching the images
-cids=('quickbird-2' 'geoeye-1' 'worldview-1' 'worldview-2' 'worldview-3' 'worldview-3-swir')
 YEARS=( $(seq $startDate 2018 ) )
-for year in "${YEARS[@]}";
-do
-   for cid in "${cids[@]}"  
-        do
-            mkdir $id/$year-$cid
-            echo CID: $cid  RANGE: $year-01-01/$year-12-31 
-            OUTPUT="$(sat-gbdx search \
-                --datetime $year-01-01/$year-12-31 \
-                --c:id $cid \
-                --intersects $id/$id.geojson  \
-                --eo:cloud_cover 0/20 \
-                --overlap 98 \
-                --save $id/$year-$cid/$year-$cid.geojson)"
-            echo $OUTPUT
-            numScenes=$(echo $OUTPUT | sed 's/[^0-9]*//g')
-            mv results.json $id/$year-$cid
-            if (( $numScenes == 0 )); then
-                rm -rf $id/$year-$cid/
-            fi
-    done
+for year in "${YEARS[@]}"; do
+     mkdir $id/$year
+     echo RANGE: $year-01-01/$year-12-31 
+     OUTPUT="$(sat-gbdx search \
+         --datetime $year-01-01/$year-12-31 \
+         --intersects $id/$id.geojson  \
+         --eo:cloud_cover 0/20 \
+         --overlap 98 \
+         --save $id/$year/scenes.geojson)"
+     echo $OUTPUT
+     numScenes=$(echo $OUTPUT | sed 's/[^0-9]*//g')
+     mv results.json $id/$year
+     if (( $numScenes == 0 )); then
+         rm -rf $id/$year/
+     fi
 done
 tar czf $id.tar.gz $id/
 mv $id.tar.gz $id/
